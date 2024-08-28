51f342efafbda31b5bcbf960bfa87eafe63314f0
Nawal Nadeem <nawal.nadeem@gmail.com>
2024-08-28 20:31:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-
-
-import turtle as t
-from turtle import Screen
-
+import colorgram
 import random
-
-
-color_list = [(245, 243, 238), (246, 242, 244), (202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
-# extracted using colorgram from downloaded image.
-
-t.colormode(255)  # 0, 255
-tim = t.Turtle()
-tim.hideturtle()
-tim.penup()
-
-tim.speed("fastest")
-
-
-tim.setheading(225)
-tim.forward(300)
-tim.setheading(0)
-
-number_of_dots = 100
-
-for dot_count in range(1, number_of_dots+1):
-   tim.dot(20, random.choice(color_list))
-   tim.forward(50)
-
-   if dot_count % 10 == 0:
-       tim.setheading(90)
-       tim.forward(50)
-       tim.setheading(180)
-       tim.forward(500)
-       tim.setheading(0)
-
-
-
+from turtle import Turtle, Screen
 
 screen = Screen()
+screen.colormode(255)
+
+turtle = Turtle()
+turtle.hideturtle()
+turtle.penup()
+
+pos_x = -295
+pos_y = -250
+turtle.teleport(pos_x, pos_y)
+turtle.speed("fastest")
+
+# extracting colours from original image
+colors = colorgram.extract("original_image_for_colour_extraction.jpg", 84)
+color_list = []
+# for reference colours come out as color_list = [(245, 243, 238), (246, 242, 244), (202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+for i in colors:
+    r = i.rgb.r
+    g = i.rgb.g
+    b = i.rgb.b
+    new_color = (r, g, b)
+    color_list.append(new_color)
+
+for i in range(1, 101):
+    color = random.choice(color_list)
+    turtle.dot(20, color)
+    turtle.fd(60)
+
+    if i % 10 == 0:
+        turtle.setheading(90)
+        turtle.fd(60)
+        pos_y += 60
+        turtle.teleport(pos_x, pos_y)
+        turtle.setheading(0)
+
 screen.exitonclick()
